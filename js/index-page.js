fb850811bf4f85a2632f8f64f73ccf621615c359
Daria Diachkova <daria.diachkova@mail.ru>
2020-11-04 20:21:45 +0300

    Set up pahes logic

---
+++ js/index-page.js
@@ -0,0 +1,4 @@
+export const IndexPage =
+  `<p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et lacus in turpis bibendum convallis egestas vitae nunc. Maecenas a hendrerit ipsum, eget pretium leo. Vivamus velit ligula, mattis at lectus et, maximus dignissim magna. Suspendisse imperdiet risus ac odio tempor, at suscipit mauris pulvinar. Quisque hendrerit quam non ante venenatis laoreet. Nulla gravida quis quam non luctus. Curabitur sit amet felis venenatis, facilisis est eu, dictum risus. Cras ullamcorper condimentum ornare. Nunc ultricies risus a euismod finibus. Fusce pulvinar, lorem et finibus scelerisque, elit lectus cursus lorem, aliquet auctor urna enim et ipsum.
+  </p>`
