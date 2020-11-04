92366b2887cccd42bad11ae0d8d7bf2e506226b9
Daria Diachkova <daria.diachkova@mail.ru>
2020-11-04 22:20:08 +0300

    Add task 7 sollution

--- js/Chapter-1/7.js
+++ js/Chapter-1/7.js
@@ -1,4 +1,7 @@
 export const Task7 = {
   text: "Find a number's square root by Newton's method.",
-  sollution: ``
+  sollution: `
+    <iframe height="400" style="width: 100%;" scrolling="no" title="SICP. Chapter 1. Task 7" src="https://codepen.io/dariadia/embed/KKMBdbE?height=265&theme-id=dark&default-tab=result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+    See the Pen <a href='https://codepen.io/dariadia/pen/KKMBdbE'>SICP. Chapter 1. Task 7</a> by dariadia
+    (<a href='https://codepen.io/dariadia'>@dariadia</a>) on <a href='https://codepen.io'>CodePen</a>.</iframe>`
 }
