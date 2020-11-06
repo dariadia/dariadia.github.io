48080ba064320665fc0da2b9aa374611b2b55723
Daria Diachkova <daria.diachkova@mail.ru>
2020-11-06 23:03:54 +0300

    Add task 11

--- js/Chapter-1/11.js
+++ js/Chapter-1/11.js
@@ -1,4 +1,7 @@
 export const Task11 = {
   text: "A function f is defined by the rule that f(n) = n if n<3 and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n > 3. Write a procedure that computes f by means of a recursive process. Write a procedure that computes f by means of an iterative process.",
-  sollution: ``
+  sollution: `<iframe height="400" style="width: 100%;" scrolling="no" title="SICP. Chapter 1. Task 11" src="https://codepen.io/dariadia/embed/NWrLgvw?height=265&theme-id=dark&default-tab=js,result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+  See the Pen <a href='https://codepen.io/dariadia/pen/NWrLgvw'>SICP. Chapter 1. Task 11</a> by dariadia
+  (<a href='https://codepen.io/dariadia'>@dariadia</a>) on <a href='https://codepen.io'>CodePen</a>.
+</iframe>`
 }
