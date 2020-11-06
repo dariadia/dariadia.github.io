e6370c6acdcded5fac44ca00b2649702b8464a7c
Daria Diachkova <daria.diachkova@mail.ru>
2020-11-06 23:04:12 +0300

    Add task 12

---
+++ js/Chapter-1/12.js
@@ -0,0 +1,7 @@
+export const Task12 = {
+  text: "The following pattern of numbers is called Pascal's triangle. The numbers at the edge of the triangle are all 1, and each number inside the triangle is the sum of the two numbers above it.35 Write a procedure that computes elements of Pascal's triangle by means of a recursive process.",
+  sollution: `<iframe height="265" style="width: 100%;" scrolling="no" title="SICP. Chapter 1. Task 12" src="https://codepen.io/dariadia/embed/ZEOMyqw?height=265&theme-id=dark&default-tab=js,result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+  See the Pen <a href='https://codepen.io/dariadia/pen/ZEOMyqw'>SICP. Chapter 1. Task 12</a> by dariadia
+  (<a href='https://codepen.io/dariadia'>@dariadia</a>) on <a href='https://codepen.io'>CodePen</a>.
+</iframe>`
+}
