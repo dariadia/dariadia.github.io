cfaeb0a0ab55d50b674544e9db238b2be34c590d
Daria Diachkova <daria.diachkova@mail.ru>
2020-11-09 17:42:24 +0300

    Change iframe height

--- js/Chapter-1/10.js
+++ js/Chapter-1/10.js
@@ -1,6 +1,6 @@
 export const Task10 = {
   text: "Calculate the Ackermann's function.",
-  sollution: `<iframe height="265" style="width: 100%;" scrolling="no" title="SICP. Chapter 1. Task 10" src="https://codepen.io/dariadia/embed/MWeBzRd?height=265&theme-id=dark&default-tab=js,result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+  sollution: `<iframe height="400" style="width: 100%;" scrolling="no" title="SICP. Chapter 1. Task 10" src="https://codepen.io/dariadia/embed/MWeBzRd?height=265&theme-id=dark&default-tab=js,result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
   See the Pen <a href='https://codepen.io/dariadia/pen/MWeBzRd'>SICP. Chapter 1. Task 10</a> by dariadia
   (<a href='https://codepen.io/dariadia'>@dariadia</a>) on <a href='https://codepen.io'>CodePen</a>.
 </iframe>`
